a2eccb8005ab36830853bb50671ce29c6ebee675
thangphan37 <pcthangk59@gmail.com>
2020-10-20 08:47:15 +0900

    Add exercise

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,7 +4,22 @@
 import React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const inputRef = React.useRef()
+  // const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState('')
   // 🐨 add a submit event handler here (`handleSubmit`).
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const {value} = inputRef.current
+    onSubmitUsername(value)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+
+    setUsername(value.toLowerCase())
+  }
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -20,12 +35,13 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input. 
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input name="username" type="text" />
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" ref={inputRef} onChange={handleChange} value={username}/>
       </div>
-      <button type="submit">Submit</button>
+      {/* {error && <div>{error}</div>} */}
+      <button type="submit" disabled={!username}>Submit</button>
     </form>
   )
 }
